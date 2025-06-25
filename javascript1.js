81646550056c1ff7e6f223e7f8ecb96a290a9780
jned1 <jaffarjned12@gmail.com>
2025-06-25 11:57:29 +0530

    first commit

---
+++ javascript1.js
@@ -0,0 +1,28 @@
+/*console.log('jaffar jned')
+console.log('jaffar jned jned')
+console.log('jaffar jned jaffar')
+const c = 30;
+var a = 10;
+let b = 20;
+a = -12.34;
+var a = 20;
+a = 70;
+b=98;
+ 
+console.log(a , b ,c);
+
+function firstName(fname){
+    console.log(fname)
+
+}
+
+ const lastName = (lname)=>{
+    console.log(lname)
+ }
+
+firstName('my name is manav');
+lastName('my lastname is jain');*/
+let arr=[2,4,6,2,5,6,78];
+console.log(arr);
+console.log(arr[0]);
+
